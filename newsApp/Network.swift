04bf9787fd1f6336502f69b27a102a8c9fa203fc
NZhukov <nzhukov.rnd@gmail.com>
2023-06-30 15:51:37 +0400

    new UI

--- newsApp/Network.swift
+++ newsApp/Network.swift
@@ -29,7 +29,6 @@ class NetworkManager {
             
             do {
                 let news = try JSONDecoder().decode(News.self, from: data)
-                print(news.articles[2].content!)
                 completion(news)
             } catch {
                 print("ошибка при декодировании \(error.localizedDescription)")
